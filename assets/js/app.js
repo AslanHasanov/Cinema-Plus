26ffef1cad96c9e380f6da26899d55d02a75d854
Aslan R. Hasanov <aslanrh@code.local>
2022-10-03 07:10:27 -0700

    initial

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+var yerler= document.querySelectorAll('.yer')
+
+yerler.forEach(yer=>{
+    yer.addEventListener('click',function(){
+        this.style.backgroundColor='rgba(93, 175, 196, 0.801)'
+    })
+
+})
